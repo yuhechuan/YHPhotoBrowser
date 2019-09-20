835aebe7f2710d7f918f2a93f77d93dfe858de68
yuhechuan <yuhechuan@kanzhun.com>
2019-09-20 11:04:11 +0800

    删除无用代码

--- Sources/YHPhotoDisplayViewController.m
+++ Sources/YHPhotoDisplayViewController.m
@@ -2,26 +2,8 @@
 //  YHPhotoDisplayViewController.m
 //  PhotoBrowser
 //
-//  Created by Roy Shaw on 8/24/15.
-//  Copyright (c) 2015 Roy Shaw (https://github.com/cuzv).
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  Created by ruaho on 2019/7/24.
+//  Copyright © 2019 ruaho. All rights reserved.
 //
 
 #import "YHPhotoDisplayViewController.h"
